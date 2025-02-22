13a4ecef7087a8a04b374a1b3c415297a23c41a8
Mr-Werwo <maxrogalski@icloud.com>
2025-02-22 17:23:48 +0100

    Kurze Beschreibung der Änderungen

--- public/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // Alle Session-Variablen löschen
-session_destroy(); // Die Session beenden
-
-// Weiterleitung zur Startseite
-header("Location: /ghg/public/index.php");
-exit();
-?>
